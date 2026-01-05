1f7114707ebb83eb47dc7f10295d6c4c39282997
Pannnomaly <supawith007@gmail.com>
2026-01-05 23:32:23 +0700

    style(ui): adjust navbar font weight and error page background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function Navbar ()
 {
     return (
-        <nav className="bg-pink-200 shadow-md text-navbar font-medium flex justify-between items-center h-20 text-2xl px-10">
-            <div className="flex items-center pl-10">
+        <nav className="bg-pink-200 shadow-md text-navbar font-bold flex justify-between items-center h-20 text-2xl px-10">
+            <div className="flex items-center">
                 <Link to = "/">
                     <div className="flex items-center gap-x-5">
                         <img src="PannProfile002.PNG" alt="Pann's logo" className="w-15" />
@@ -14,7 +14,7 @@ export default function Navbar ()
                     </div>
                 </Link>
             </div>
-            <div className=" flex items-center gap-x-5 pr-10">
+            <div className=" flex items-center gap-x-5">
                 <div className="px-5 py-3 rounded-md hover:bg-pink-300 hover:text-pink-100 transition duration-300 ease-in-out cursor-pointer">
                     <Link to = "developer_acts">
                         <p>The Acts of the Developers</p>
